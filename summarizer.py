518039183fb68e0841aaa21ff351e9bd9de5b1c6
davidlowryduda <david.j.lowry@gmail.com>
2017-02-25 07:00:22 +0000

    Add brief documentation header to summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,5 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Produces an automated summary of the webpage.
+
+Using the LSA Summarizer from sumy, this attempts to create an automated
+summarizer of the content at a given url. There are very many unhandled
+edge-cases, so use with some caution.
+
+WARNING
+-------
+  For some pages, this summarizer can hang. So this should be run with
+  some other memory management tools.
+
+
+
+COPYRIGHT NOTICE
+---------------
+Copyright 2016-2017 David Lowry-Duda
+
+You are free to redistribute and/or modify HNRSS under the
+terms of the MIT License. A copy of this license should be made available with
+the source.
+
+I'm happy if you find HNRSS useful, but be advised that
+it comes WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+"""
+
 
 from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
